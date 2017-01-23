e585e78fdf41eee154bd88a579b66e16e3263907
Qiu Juntao <juntao.qiu@gmail.com>
2017-01-23 17:43:28 +1100

    update the style a little bit

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ $(function() {
 	var container = $('.container');
 
 	$.when(data, tmpl).done(function(data, template) {
+		console.time('rendering');
 		var compiled = _.template(template[0]);	
 		data[0].forEach(function(item, index) {
 			var html = compiled(item);
 			container.append(html);
 		});
+		console.timeEnd('rendering');
 	})
+
+	container.on('click', '.simple-card', function() {
+		console.log($('.card-title', this).text().trim());
+	});
 });
