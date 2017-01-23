bd49c8b37829615ee8c0b6e830f8aa9b6af30247
Qiu Juntao <juntao.qiu@gmail.com>
2017-01-23 20:33:16 +1100

    add vue demo

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app" class="container">
+    <cards></cards>
+  </div>
+</template>
+
+<script>
+import Cards from './components/Cards'
+
+export default {
+  name: 'app',
+  components: {
+    Cards
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  width: 800px;
+  margin: 40px auto;
+}
+
+.card-type {
+  background-color: rgb(230, 126, 34);
+}
+
+.column-card {
+  padding: 6px;
+  border: 1px dotted #ccc;
+  margin: 6px 0;
+  background-color: #efefef;
+  color: #333;
+}
+
+.card-title {
+  font-weight: thin;
+  padding: 4px 0;
+}
+
+.card-owner {
+  font-style: italic;
+}
+
+</style>
---
+++ vue-demo/src/components/Cards.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <card v-for="item in items" v-bind:item="item">
+        </card>
+    </div>
+</template>
+
+<script>
+import Card from './Card'
+
+export default {
+  name: 'cards',
+  data () {
+    return {
+        items: []
+    }
+  },
+  mounted () {
+    var self = this;
+    this.$http.get('/static/data/cards.json').then((response) => {
+        self.items = response.data;
+      }, (error) => {
+        console.log(JSON.stringify(error));
+      });
+  },
+  components: {
+    Card
+  }
+}
+</script>
