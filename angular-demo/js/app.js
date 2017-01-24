f9081ad100520e81a184118113f7c8094ad2e8cb
Qiu Juntao <juntao.qiu@gmail.com>
2017-01-24 16:57:43 +1100

    add dnd

--- angular-demo/js/app.js
+++ angular-demo/js/app.js
@@ -1,5 +1,5 @@
 (function() {
- var app =  angular.module('app', []);
+ var app =  angular.module('app', ['dndLists']);
  
   app.controller('HomeCtrl', function() {
   });
--- angular-demo/js/components/cards.js
+++ angular-demo/js/components/cards.js
@@ -4,18 +4,21 @@
   var app = angular.module('app');
 
   app.component('cards', {
-    // Binds caption to the parent scope
     bindings: {
       caption: '<'
     },
-    // Loads the component template
+
     templateUrl: '/js/components/cards.html',
+    
     controller: function ($http) {
       this.cards = [];
       var self = this;
       $http.get('/data/cards.json').success(function(cards) {
         self.cards = cards;
       });
+
+      var drag = function() {};
+      var drop = function() {};
     }
   });
 })();
